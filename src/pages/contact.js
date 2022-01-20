5f24ffe1291b7f95935edae3918f63f5eb207528
James OKeeffe <jamesokeeffe50@gmail.com>
2022-01-20 22:05:52 +0800

    Further play around with basic Gatsby features

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+  return (
+    <Layout pageTitle="Contact">
+      <p>Here is my contact information.</p>
+    </Layout>
+  )
+}
+
+export default ContactPage
