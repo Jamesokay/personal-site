5ddf47d3f71a68cbef4daed21f52ae2483b0d3b1
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-05 20:22:45 +0800

    More responsivity on portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { container, topPanel, titleContainer, title, titleSub, videoContainer, video, quote, text, features, featureBox, featureImage, featureTextBox, featureText } from '../styles/portfolio.module.css'
+import { container, topPanel, titleContainer, title, titleSub, videoContainer, video, quote, text, features, featureBox, featureTitle, featureImage, featureTextBox, featureText } from '../styles/portfolio.module.css'
 
 export default function portfolio() {
     return (
@@ -40,16 +40,24 @@ export default function portfolio() {
         </div>
           
           <div className={features}>
+            <h1 className={titleSub}>Features</h1>
+            
             <div className={featureBox}>
               <div className={featureImage} />
-              <div className={featureTextBox}> 
+              <div className={featureTextBox}>
+              <p className={featureTitle}>Feature Title</p>
               <span className={featureText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-              deserunt mollit anim id est laborum.
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+              architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+              sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit 
+              amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut 
+              labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, 
+              quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea 
+              commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
+              esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas 
+              nulla pariatur?
               </span>
               
               </div>
@@ -57,14 +65,20 @@ export default function portfolio() {
 
             <div className={featureBox}>
               <div className={featureImage} />
-              <div className={featureTextBox}> 
+              <div className={featureTextBox}>
+              <p className={featureTitle}>Feature Title</p>
               <span className={featureText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-              deserunt mollit anim id est laborum.
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+              architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+              sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit 
+              amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut 
+              labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, 
+              quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea 
+              commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
+              esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas 
+              nulla pariatur?
               </span>
               
               </div>
@@ -72,14 +86,20 @@ export default function portfolio() {
 
             <div className={featureBox}>
               <div className={featureImage} />
-              <div className={featureTextBox}> 
+              <div className={featureTextBox}>
+              <p className={featureTitle}>Feature Title</p>
               <span className={featureText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-              deserunt mollit anim id est laborum.
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+              architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+              sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit 
+              amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut 
+              labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, 
+              quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea 
+              commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
+              esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas 
+              nulla pariatur?
               </span>
               
               </div>
