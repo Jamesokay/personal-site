0c033f59188447e44c5e3300bec256d5a7633208
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-09 08:58:41 +0800

    Experimenting with content fade in

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import Layout from "../components/Layout"
-import { container, contactDetails, contactDetailsItem } from '../styles/contact.module.css'
+import { container, greeting, contactDetails, contactDetailsItem } from '../styles/contact.module.css'
 
 const ContactPage = () => {
   return (
     <Layout>
       <div className={container}>
+        <h1 className={greeting}>Hello</h1>
         <ul className={contactDetails}>
           <li className={contactDetailsItem}>0426 221 594</li>
           <li className={contactDetailsItem}>jamesokeeffe50@gmail.com</li>
