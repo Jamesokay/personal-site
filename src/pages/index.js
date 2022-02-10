bfe3b2760edf25cd87cfe7d3981b1680463ca23d
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-10 09:36:37 +0800

    Playing with styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
-import { container, textContainer, quote, italic } from '../styles/home.module.css'
+import { container, textContainer, name, quote, italic } from '../styles/home.module.css'
 
 const IndexPage = () => {
   return (
     <Layout>
       <div className={container}>
+        <h1 className={name}>James O'Keeffe</h1>
         <div className={textContainer}>
           <span className={quote}>
-            I am a full-stack developer passionate about creating web applications 
-            that are both <span className={italic}>functional</span> and <span className={italic}>elegant</span>
+            Full-stack developer passionate about creating web applications 
+            that are both <span className={italic}>functional</span> & <span className={italic}>elegant</span>
           </span>
         </div>
       </div>
