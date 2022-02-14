fe91405ca4e3a8fe991cb2af901f4b6eed13bd52
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-14 17:39:31 +0800

    Simplification of landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
-import { StaticImage } from "gatsby-plugin-image"
-import { container, textContainer, name, image, quote, italic } from '../styles/home.module.css'
+import { container, textContainer, quote, italic } from '../styles/home.module.css'
 
 const IndexPage = () => {
   return (
     <Layout>
       <div className={container}>
-        <h1 className={name}>James O'Keeffe</h1>
         <div className={textContainer}>
-          <StaticImage className={image} src='../images/myface.jpg' alt='Photo of me' />
           <span className={quote}>
-            Full-stack developer passionate about creating web applications 
+            I'm James, a full-stack developer creating web applications 
             that are both <span className={italic}>functional</span> & <span className={italic}>elegant</span>
           </span>
         </div>
