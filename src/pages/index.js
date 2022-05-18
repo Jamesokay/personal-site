091a2e18001452e075f40d0bb927a27abcd78242
James OKeeffe <jamesokeeffe50@gmail.com>
2022-05-18 14:40:12 +0800

    Added headshot

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout'
 import Portfolio from '../components/Portfolio'
 import Contact from '../components/Contact'
+import myHead from '../images/myHead.png'
 import { container, navBar, navOption, containerMid, containerMidLeft, containerSmall, containerSmallTextBox, containerSmallText, containerSmallTextLarge, textContainer, photoContainer, quote } from '../styles/home.module.css'
 import scrollTo from 'gatsby-plugin-smoothscroll'
 
@@ -82,7 +83,7 @@ const IndexPage = () => {
           </div>
          </div>
 
-          <div className={`js-scroll ${photoContainer}`} />
+          <img src={myHead} className={`js-scroll ${photoContainer}`} alt='' />
         </div>
         <Portfolio />
         </div>
