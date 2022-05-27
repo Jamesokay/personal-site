fd71d6ec0c360221203f4aef0174ff236a75f323
James OKeeffe <jamesokeeffe50@gmail.com>
2022-05-27 15:15:45 +0800

    Updated contact details

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,6 @@ export default function Contact() {
       <div className='contactSection'>
         <h1 className={`js-scroll ${greeting}`}>Hello</h1>
         <ul className={`js-scroll ${contactDetails}`}>
-          <li className={contactDetailsItem}>0426 221 594</li>
           <li className={contactDetailsItem}>jamesokeeffe50@gmail.com</li>
           <li className={contactDetailsItem}><a className={contactDetailsLink} href='https://github.com/Jamesokay' target='_blank'>GitHub</a></li>
           <li className={contactDetailsItem}><a className={contactDetailsLink} href='https://www.linkedin.com/in/james-o-keeffe-283881170/' target='_blank'>LinkedIn</a></li>
